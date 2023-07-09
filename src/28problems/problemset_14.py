6dbba61041d8a984dd8ee6c6db2ebda74a461ac1
AlexanderLolo <filippov.alex.u@gmail.com>
2023-07-09 11:11:12 +0300

    added N of traffic lisghts

--- src/28problems/problemset_14.py
+++ src/28problems/problemset_14.py
@@ -1,14 +1,23 @@
 def Unmanned(L: int, N: int, track: list) -> int:
+# Определяем количество светофоров на пути
+    num_of_traf = 0
+    for i in range(N):
+        if track[i][0] >= L:
+            break
+        num_of_traf += 1
 
-    if N == 0:
+    if num_of_traf == 0:
         return L
+# Вычисляем время передвижения
 
+# Время прохождения  до первого светофора включительно
     time = track[0][0] + Waitingtime(track[0][0], track[0])
 
-    for i in range(1, N):
+# Время прохождения  до конца маршрута
+    for i in range(1, num_of_traf):
         time += track[i][0] - track[i-1][0]
         time += Waitingtime(time, track[i])
-    return time + L - track[-1][0]
+    return time + L - track[num_of_traf-1][0]
 
 
 def Waitingtime(time: int, regime: list) -> int:
--- src/28problems/test_14.py
+++ src/28problems/test_14.py
@@ -10,8 +10,12 @@ class Problem14_test(unittest.TestCase):
         self.assertEqual(Unmanned(10, 3, [[3, 5, 5], [5, 2, 2], [6, 1, 1]]), 13)
         self.assertEqual(Unmanned(10, 3, [[3, 5, 5], [5, 2, 2], [6, 1, 2]]), 12)
         self.assertEqual(Unmanned(10, 0, []), 10)
-        self.assertEqual(Unmanned(0, 0, []), 0)
-        self.assertEqual(Unmanned(3, 1, [[3, 5, 5]]), 5)
+        self.assertEqual(Unmanned(1, 0, []), 1)
+        self.assertEqual(Unmanned(3, 1, [[3, 5, 5]]), 3)
+        self.assertEqual(Unmanned(10, 2, [[11, 5, 5], [15, 2, 2]]), 10)
+        self.assertEqual(Unmanned(10, 3, [[3, 5, 5], [5, 2, 2], [11, 5, 5]]), 12)
+        self.assertEqual(Unmanned(4, 1, [[3, 5, 5]]), 6)
+        self.assertEqual(Unmanned(4, 2, [[3, 5, 5], [5, 2, 2]]), 6)
 
 
 if __name__ == "__main__":
