118875819dd73149d5c25d2f9b64fb9d347b9919
AlexanderLolo <filippov.alex.u@gmail.com>
2023-07-11 01:17:39 +0300

    a15

--- src/28problems/problemset_15.py
+++ src/28problems/problemset_15.py
@@ -1,21 +1,27 @@
-def TankRush(H1: int, W1: int, S1: str, H2: int, W2: int, S2: str,) -> bool:
-    
+def TankRush(H1: int, W1: int, field1: str, H2: int, W2: int, field2: str) -> bool:
+
     if H2 > H1 or W2 > W1:
         return False
     
-    field1 = S1.split()
-    field2 = S2.split()
-    
-    offset = 0
-    for row2 in field2:
-        if offset > H1:
-            return False
-        
-        for i, row1 in enumerate(field1[offset:]):
-            if row2 in row1:
-                break
-            if i == len(field1[offset:])-1:
-                return False
-        offset += i+1
+    S1 = field1.replace(" ", "")
+    S2 = field2.replace(" ", "")
 
-    return True
+# ищем малый массив во всех строчках большого массива
+    for i in range(H1-H2+1):
+        if TankRushHelp(W1, S1[i*W1:], H2, W2, S2):
+            return True
+    return False
+
+
+def TankRushHelp(W1: int, S1: str, H2: int, W2: int, S2: str) -> bool:
+# ищем малый массив в первых H2 строчках большого массива
+    if S1 == S2:
+        return True
+    
+    for j in range(W1-W2+1):
+        b = ""
+        for i in range(j, H2*W1+j, W1):
+            b += S1[i:i+W2]
+        if b == S2:
+            return True
+    return False
--- src/28problems/test_15.py
+++ src/28problems/test_15.py
@@ -5,15 +5,14 @@ from problemset_15 import TankRush
 class Problem15_test(unittest.TestCase):
 
     def test_regression(self):
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 2, "34 98"), True)
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 2, "12 23"), True)
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 2, "45 87"), True)
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 1, 1, "45"), True)
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 1, 1, ""), True)
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 3, 4, "1234 2345 0985"), False)
-        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 2, "1234 0987"), True)
+        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 3, 2, "23 34 98"), True)
+        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 1, "2 3"), True)
+        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 1, "5 7"), True)
+        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 4, "1234 2345"), True)
         self.assertEqual(TankRush(0, 0, "", 0, 0, ""), True)
-        
+        self.assertEqual(TankRush(3,3, '321 694 798', 2, 2, '69 98'), False)
 
+
+        
 if __name__ == "__main__":
     unittest.main()
