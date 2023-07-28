60f6304f47ad85db17800cd16197537e11be2d2e
AlexanderLolo <filippov.alex.u@gmail.com>
2023-07-28 21:41:47 +0300

    a

---
+++ src/28problems/problemset_26.py
@@ -0,0 +1,21 @@
+def white_walkers(village: str) -> bool:
+
+    summ = 0
+    count = 0
+    flag = False
+    for char in village:
+
+        if char == "=":
+            count += 1
+
+        if char.isdigit() and summ + int(char) == 10:
+            summ = int(char)
+            flag = True
+            if count == 3:
+                count = 0
+                continue
+            return False
+        
+        elif char.isdigit():
+            summ = int(char)
+    return flag
---
+++ src/28problems/test_26.py
@@ -0,0 +1,15 @@
+import unittest
+from problemset_26 import white_walkers
+
+
+class Problem26_test(unittest.TestCase):
+
+    def test_regression(self):
+        self.assertEqual(white_walkers("axxb6===4xaf5===eee5"), True)
+        self.assertEqual(white_walkers("5==ooooooo=5=5"), False)
+        self.assertEqual(white_walkers("abc=7==hdjs=3gg1=======5"), False)
+        self.assertEqual(white_walkers("aaS=8"), False)
+        self.assertEqual(white_walkers("9===1===9===1===9"), True)
+        
+if __name__ == "__main__":
+    unittest.main()
