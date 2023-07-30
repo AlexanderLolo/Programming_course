c72bdd8ea4be213298c511b75c60f1443390b990
AlexanderLolo <filippov.alex.u@gmail.com>
2023-07-30 11:47:33 +0300

    27

--- src/28problems/problemset_27.py
+++ src/28problems/problemset_27.py
@@ -1,9 +1,13 @@
 def Football(flist: list, N: int) -> bool:
 
     fordered = sorted(flist)
+
+    if fordered == flist or N == 1:
+        return False
+
     diff = list(map(lambda x, y: 1 if x-y != 0 else 0, flist, fordered))
 
-    if diff.count(1) in [0, 2]:
+    if diff.count(1) == 2:
         return True
 
     lower = diff.index(1)
--- src/28problems/test_27.py
+++ src/28problems/test_27.py
@@ -6,6 +6,8 @@ class Problem27_test(unittest.TestCase):
 
     def test_regression(self):
         self.assertEqual(Football([1, 3, 2], 3), True)
+        self.assertEqual(Football([1], 1), False)
+        self.assertEqual(Football([1, 2, 3], 3), False)
         self.assertEqual(Football([3, 1, 1], 3), True)
         self.assertEqual(Football([1, 7, 5, 3, 9], 5), True)
         self.assertEqual(Football([9, 5, 3, 7, 1], 5), False)
