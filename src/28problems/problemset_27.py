b71c918e3e177522e5692e88fdbb6861cf4249fb
AlexanderLolo <filippov.alex.u@gmail.com>
2023-07-30 11:25:52 +0300

    27

---
+++ src/28problems/problemset_27.py
@@ -0,0 +1,12 @@
+def Football(flist: list, N: int) -> bool:
+
+    fordered = sorted(flist)
+    diff = list(map(lambda x, y: 1 if x-y != 0 else 0, flist, fordered))
+
+    if diff.count(1) in [0, 2]:
+        return True
+
+    lower = diff.index(1)
+    upper = N - diff[::-1].index(1)
+
+    return fordered[lower:upper] == reversed(flist[lower:upper])
---
+++ src/28problems/test_27.py
@@ -0,0 +1,16 @@
+import unittest
+from problemset_27 import Football
+
+
+class Problem27_test(unittest.TestCase):
+
+    def test_regression(self):
+        self.assertEqual(Football([1, 3, 2], 3), True)
+        self.assertEqual(Football([3, 1, 1], 3), True)
+        self.assertEqual(Football([1, 7, 5, 3, 9], 5), True)
+        self.assertEqual(Football([9, 5, 3, 7, 1], 5), False)
+        self.assertEqual(Football([1, 4, 3, 2, 5], 5), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
