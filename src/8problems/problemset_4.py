49b62bd7048ae0ed5cbbf733f0507fca538bbebb
AlexanderLolo <filippov.alex.u@gmail.com>
2023-08-05 22:38:49 +0300

    4

--- src/8problems/problemset_4.py
+++ src/8problems/problemset_4.py
@@ -1,6 +1,19 @@
-def Palindrom(string1: str) -> bool:
+def palindrom(string1: str) -> bool:
 
     if len(string1) == 1 or len(string1) == 0:
         return True
-    
-    return Palindrom(string1[1:-1]) and string1[0] == string1[-1]
+
+    count = 0
+    length = len(string1)
+    return palicheck(string1, count, length)
+
+
+def palicheck(string1, count, length):
+
+    if string1[count] != string1[length-count-1]:
+        return False
+
+    if length == (count + 1) * 2 or length == (count + 1) * 2 + 1:
+        return True
+
+    return palicheck(string1, count + 1, length)
