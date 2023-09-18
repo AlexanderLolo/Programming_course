5eb5dab1943494cfa01ca61269699ae8fc95e8bc
AlexanderLolo <filippov.alex.u@gmail.com>
2023-09-18 23:38:15 +0300

    8

---
+++ src/Algorithms_1/problemset_8_hashtables.py
@@ -0,0 +1,43 @@
+class Hashtable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+
+        return len(value.encode("utf-8")) % self.size
+
+    def seek_slot(self, value):
+
+        pos = self.hash_fun(value)
+        if self.slots[pos] is None:
+            return pos
+
+        newpos = pos + self.step
+        while newpos % self.size != pos:
+            if self.slots[newpos] is None:
+                return newpos
+            newpos = (newpos + self.step) % self.size
+        return None
+
+    def put(self, value):
+
+        pos = self.seek_slot(value)
+        if pos is None:
+            return None
+        self.slots[pos] = value
+        return pos
+
+    def find(self, value):
+
+        pos = self.hash_fun(value)
+        if self.slots[pos] == value:
+            return pos
+
+        newpos = pos + self.step
+        while newpos % self.size != pos and self.slots[newpos] is not None:
+            if self.slots[newpos] == value:
+                return newpos
+            newpos = (newpos + self.step) % self.size
+        return None
---
+++ src/Algorithms_1/test_8.py
@@ -0,0 +1,20 @@
+import unittest
+from problemset_8_hashtables import Hashtable
+
+
+class Hashtable_test(unittest.TestCase):
+
+    def test_regression(self):
+        hashtable = Hashtable(17, 3)
+        a = []
+        for i in range(17):
+            a.append(hashtable.put(str(i)))
+        self.assertEqual(hashtable.put(str(17)), None)
+
+        for i, element in enumerate(a):
+            self.assertEqual(hashtable.find(str(i)), element)
+        self.assertEqual(hashtable.find("18"), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
